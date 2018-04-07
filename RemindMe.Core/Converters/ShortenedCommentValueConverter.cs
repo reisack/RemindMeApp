b37aa79db98b4287b4d891c445b8158a2beae10c
Rémi Eisack <remi.eisack@gmail.com>
2018-04-07 20:47:28 +0200

    Shortened comment converter

---
+++ RemindMe.Core/Converters/ShortenedCommentValueConverter.cs
@@ -0,0 +1,28 @@
+﻿using MvvmCross.Platform.Converters;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace RemindMe.Core.Converters
+{
+    public class ShortenedCommentValueConverter : MvxValueConverter<string, string>
+    {
+        private const int MESSAGE_LENGTH = 30;
+
+        protected override string Convert(string value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Removing line breaks
+            string newValue = value.Replace(System.Environment.NewLine, " ");
+
+            if (newValue.Length > MESSAGE_LENGTH)
+            {
+                return newValue.Substring(0, MESSAGE_LENGTH) + "...";
+            }
+            else
+            {
+                return newValue;
+            }
+        }
+    }
+}
