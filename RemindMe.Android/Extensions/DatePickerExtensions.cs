9a59bc6f1e419371bd124ba79a56e5c823a57693
Rémi Eisack <remi.eisack@gmail.com>
2018-04-25 20:13:45 +0200

    Android 4.4 issue : Datepicker minimum date must be strictly inferior to date now. Solution : remove 1 second to the date

--- RemindMe.Android/Extensions/DatePickerExtensions.cs
+++ RemindMe.Android/Extensions/DatePickerExtensions.cs
@@ -22,8 +22,10 @@ namespace RemindMe.Android.Extensions
             if (dt.CompareTo(javaMinDt) < 0)
                 throw new ArgumentException("Must be >= Java's min DateTime of 1/1970");
 
-            var longVal = dt - javaMinDt;
-            picker.MinDate = (long)longVal.TotalMilliseconds;
+            var longVal = dt.ToUniversalTime() - javaMinDt;
+            // Android 4.4 bug : We substract one second to the date, because minimum date
+            // must be before now and not strictly equal to now.
+            picker.MinDate = (long)longVal.TotalMilliseconds - 1000;
         }
 
         public static void SetMaxDate(this DatePicker picker, DateTime dt)
@@ -32,7 +34,7 @@ namespace RemindMe.Android.Extensions
             if (dt.CompareTo(javaMinDt) < 0)
                 throw new ArgumentException("Must be >= Java's min DateTime of 1/1970");
 
-            var longVal = dt - javaMinDt;
+            var longVal = dt.ToUniversalTime() - javaMinDt;
             picker.MaxDate = (long)longVal.TotalMilliseconds;
         }
     }
