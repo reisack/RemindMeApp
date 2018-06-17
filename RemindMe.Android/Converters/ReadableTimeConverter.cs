b98aed0945a1d651a472989038b49aac63ed2393
Rémi Eisack <remi.eisack@gmail.com>
2018-06-17 15:31:46 +0200

    Remove commented code

--- RemindMe.Android/Converters/ReadableTimeConverter.cs
+++ RemindMe.Android/Converters/ReadableTimeConverter.cs
@@ -16,29 +16,10 @@ namespace RemindMe.Android.Converters
             if (!DateFormat.Is24HourFormat(Application.Context))
             {
                 return DateFormat.Format("h:mm a", date);
-                //string amOrPm = "am";
-
-                //if (hours == 0)
-                //{
-                //    hours = 12;
-                //    amOrPm = "am";
-                //}
-                //else if (hours == 12)
-                //{
-                //    amOrPm = "pm";
-                //}
-                //else if (hours > 12)
-                //{
-                //    hours -= 12;
-                //    amOrPm = "pm";
-                //}
-
-                //return $"{hours}:{minutes.ToString("00")} {amOrPm}";
             }
             else
             {
                 return DateFormat.Format("HH:mm", date);
-                //return $"{hours.ToString("00")}:{minutes.ToString("00")}";
             }
         }
     }
--- RemindMe.Android/Converters/TimestampToReadableTimeValueConverter.cs
+++ RemindMe.Android/Converters/TimestampToReadableTimeValueConverter.cs
@@ -14,40 +14,18 @@ namespace RemindMe.Android.Converters
             DateTimeOffset dateTimeOffset = DateTimeOffset.FromUnixTimeSeconds(value);
             DateTime localDate = dateTimeOffset.LocalDateTime;
 
-            int hours = localDate.Hour;
-            int minutes = localDate.Minute;
-
             // For this date, we only care about hour and minute
             // We set the date on the first of January 2000 as a dummy value
             // This is faster than doing GetYear(), GetMonth(), etc...
-            Date date = new Date(2000, 1, 1, hours, minutes, 0);
+            Date date = new Date(2000, 1, 1, localDate.Hour, localDate.Minute, 0);
 
             if (!DateFormat.Is24HourFormat(Application.Context))
             {
                 return DateFormat.Format("h:mm a", date);
-                //string amOrPm = "am";
-
-                //if (hours == 0)
-                //{
-                //    hours = 12;
-                //    amOrPm = "am";
-                //}
-                //else if (hours == 12)
-                //{
-                //    amOrPm = "pm";
-                //}
-                //else if (hours > 12)
-                //{
-                //    hours -= 12;
-                //    amOrPm = "pm";
-                //}
-
-                //return $"{hours}:{minutes.ToString("00")} {amOrPm}";
             }
             else
             {
                 return DateFormat.Format("HH:mm", date);
-                //return $"{hours.ToString("00")}:{minutes.ToString("00")}";
             }
         }
     }
