1741e09701049471a7b8a099f515a81c4c3664e9
Rémi Eisack <remi.eisack@gmail.com>
2018-11-24 02:12:57 +0100

    Refactoring

--- RemindMe.Android/Events/BootReceiver.cs
+++ RemindMe.Android/Events/BootReceiver.cs
@@ -20,14 +20,11 @@ namespace RemindMe.Android
                     || intent.Action == "android.intent.action.QUICKBOOT_POWERON"
                     || intent.Action == "com.htc.intent.action.QUICKBOOT_POWERON")
                     {
-                        if (context != null)
+                        if (context.GetSystemService(Context.NotificationService) is NotificationManager notificationManager)
                         {
-                            if (context.GetSystemService(Context.NotificationService) is NotificationManager notificationManager)
-                            {
-                                ReminderService.Instance.NotifyReminders(notificationManager, context);
-                            }
-                            ReminderService.Instance.StartOrWakeUpService(context);
+                            ReminderService.Instance.NotifyReminders(notificationManager, context);
                         }
+                        ReminderService.Instance.StartOrWakeUpService(context);
                     }
                 }
             }
